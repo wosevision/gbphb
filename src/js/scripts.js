d2de7eaabb1666f25ed6792519f6d1ed46ce2e2e
Wose <Jacksonteather@hotmail.com>
2017-04-16 19:01:30 -0400

    WIP add Swiper gallery: styles and JS included

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,9 +7,20 @@
   // document ready
   $(function () {
 
-    var swiper = new Swiper('.swiper-container', {
-			loop: true
+    var galleryTop = new Swiper('.gallery-top', {
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 10,
     });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        touchRatio: 0.2,
+        slideToClickedSlide: true
+    });
+    galleryTop.params.control = galleryThumbs;
+    galleryThumbs.params.control = galleryTop;
 
   });
 
