579420f1bffb907033acc6d6c99e4da7dff59841
Wose <Jacksonteather@hotmail.com>
2017-05-14 00:43:15 -0400

    ⚡️ Add constants to gallery init calls for easy class switching

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,36 +5,48 @@
 
   'use strict';
 
-  var galleryConfigs = {
-    '.gallery-large .gallery-images': {
-      loop: true,
-      nextButton: '.gallery-large .swiper-button-next',
-      prevButton: '.gallery-large .swiper-button-prev',
-      pagination: '.gallery-large .swiper-pagination',
-      paginationType: 'custom',
-      paginationCustomRender: paginationCustomRender,
-      spaceBetween: 10,
-      a11y: true
-    },
-    '.gallery-large .gallery-cards': {
-      loop: true,
-      parallax: true,
-      touchRatio: 0.6,
-      effect: 'fade',
-      a11y: true
-    },
-    '.gallery-small .gallery-cards': {
-      grabCursor: true,
-      nextButton: '.gallery-small .swiper-button-next',
-      prevButton: '.gallery-small .swiper-button-prev',
-      pagination: '.gallery-small .swiper-pagination',
-      slidesPerView: 'auto',
-      paginationClickable: true,
-      paginationType: 'custom',
-      paginationCustomRender: paginationCustomRender,
-      spaceBetween: 16
-    }
+  var galleries = {},
+      galleryConfigs = {};
+  var galleryLarge = '.gallery-large',
+      gallerySmall = '.gallery-small';
+  var galleryLargeImages = join(galleryLarge, '.gallery-images'),
+      galleryLargeCards = join(galleryLarge, '.gallery-cards'),
+      gallerySmallCards = join(gallerySmall, '.gallery-cards');
+  
+  galleryConfigs[galleryLargeImages] = {
+    loop: true,
+    nextButton: join(galleryLarge, '.swiper-button-next'),
+    prevButton: join(galleryLarge, '.swiper-button-prev'),
+    pagination: join(galleryLarge, '.swiper-pagination'),
+    paginationType: 'custom',
+    paginationCustomRender: paginationCustomRender,
+    spaceBetween: 10,
+    a11y: true
   };
+  galleryConfigs[galleryLargeCards] = {
+    loop: true,
+    parallax: true,
+    touchRatio: 0.6,
+    effect: 'fade',
+    a11y: true
+  };
+  galleryConfigs[gallerySmallCards] = {
+    grabCursor: true,
+    nextButton: join(gallerySmall, '.swiper-button-next'),
+    prevButton: join(gallerySmall, '.swiper-button-prev'),
+    pagination: join(gallerySmall, '.swiper-pagination'),
+    slidesPerView: 'auto',
+    paginationClickable: true,
+    paginationType: 'custom',
+    paginationCustomRender: paginationCustomRender,
+    spaceBetween: 16
+  };
+
+  function join(parts) {
+    var args = [];
+    for (var i = 0; i < arguments.length; ++i) args[i] = arguments[i];
+    return args.join(' ');
+  }
 
   function prependZero(digit) {
     return digit < 10 ? ('0' + digit) : digit;
@@ -45,13 +57,11 @@
   }
 
   function initStuff() {
-    var galleries = {};
     for (var config in galleryConfigs) {
       galleries[config] = new Swiper(config, galleryConfigs[config]);
     }
-
-    galleries['.gallery-large .gallery-images'].params.control = galleries['.gallery-large .gallery-cards'];
-    galleries['.gallery-large .gallery-cards'].params.control = galleries['.gallery-large .gallery-images'];
+    galleries[galleryLargeImages].params.control = galleries[galleryLargeCards];
+    galleries[galleryLargeCards].params.control = galleries[galleryLargeImages];
   }
 
   // document ready
