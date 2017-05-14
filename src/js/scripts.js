715e23bec21377f0c05249d3e14793e17256ef42
Wose <Jacksonteather@hotmail.com>
2017-05-14 00:21:48 -0400

    🚿 Rearranged script file to be more human readable

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,40 @@
 //=require jquery/dist/jquery.js 
 //=require swiper/dist/js/swiper.js 
 
-(function ($, window, document, undefined) {
+(function ($, Swiper, window, document, undefined) {
 
   'use strict';
+
+  var galleryConfigs = {
+    '.gallery-large .gallery-images': {
+      loop: true,
+      nextButton: '.gallery-large .swiper-button-next',
+      prevButton: '.gallery-large .swiper-button-prev',
+      pagination: '.gallery-large .swiper-pagination',
+      paginationType: 'custom',
+      paginationCustomRender: paginationCustomRender,
+      spaceBetween: 10,
+      a11y: true
+    },
+    '.gallery-large .gallery-cards': {
+      loop: true,
+      parallax: true,
+      touchRatio: 0.6,
+      effect: 'fade',
+      a11y: true
+    },
+    '.gallery-small .gallery-cards': {
+      grabCursor: true,
+      nextButton: '.gallery-small .swiper-button-next',
+      prevButton: '.gallery-small .swiper-button-prev',
+      pagination: '.gallery-small .swiper-pagination',
+      slidesPerView: 'auto',
+      paginationClickable: true,
+      paginationType: 'custom',
+      paginationCustomRender: paginationCustomRender,
+      spaceBetween: 16
+    }
+  };
 
   function prependZero(digit) {
     return digit < 10 ? ('0' + digit) : digit;
@@ -14,40 +45,16 @@
   }
 
   function initStuff() {
-    var galleryLargeImages = new Swiper('.gallery-large .gallery-images', {
-        loop: true,
-        nextButton: '.gallery-large .swiper-button-next',
-        prevButton: '.gallery-large .swiper-button-prev',
-        pagination: '.gallery-large .swiper-pagination',
-        paginationType: 'custom',
-        paginationCustomRender: paginationCustomRender,
-        spaceBetween: 10,
-        a11y: true,
-    });
-    var galleryLargeCards = new Swiper('.gallery-large .gallery-cards', {
-        loop: true,
-        parallax: true,
-        touchRatio: 0.6,
-        effect: 'fade',
-        a11y: true,
-    });
-    galleryLargeImages.params.control = galleryLargeCards;
-    galleryLargeCards.params.control = galleryLargeImages;
+    var galleries = {};
+    for (var config in galleryConfigs) {
+      galleries[config] = new Swiper(config, galleryConfigs[config]);
+    }
 
-    var gallerySmall = new Swiper('.gallery-small .gallery-cards', {
-      grabCursor: true,
-      nextButton: '.gallery-small .swiper-button-next',
-      prevButton: '.gallery-small .swiper-button-prev',
-      pagination: '.gallery-small .swiper-pagination',
-      slidesPerView: 'auto',
-      paginationClickable: true,
-      paginationType: 'custom',
-      paginationCustomRender: paginationCustomRender,
-      spaceBetween: 16
-    });
+    galleries['.gallery-large .gallery-images'].params.control = galleries['.gallery-large .gallery-cards'];
+    galleries['.gallery-large .gallery-cards'].params.control = galleries['.gallery-large .gallery-images'];
   }
 
   // document ready
   $(initStuff);
 
-})(jQuery, window, document);
+})(jQuery, Swiper, window, document);
