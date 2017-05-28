7479b17c6660a6b2232b40e60403ab66b4c326d9
Wose <Jacksonteather@hotmail.com>
2017-05-28 02:28:52 -0400

    ⚡️ WIP add JS class for nav parallax / scrolly math

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -61,18 +61,65 @@ class Page {
     this.galleries[galleryLargeImages].params.control = this.galleries[galleryLargeCards];
     this.galleries[galleryLargeCards].params.control = this.galleries[galleryLargeImages];
 	}
+}
+
+class Nav {
+	constructor({
+		container,
+		toggler,
+		toggleClass,
+		background,
+		bgSpeed
+	}) {
+		this.$window = $(window);
+		this.$container = $(container);
+		this.$toggler = $(toggler);
+		this.$background = $(background);
+
+		this.toggleClass = toggleClass;
+		this.bgSpeed = bgSpeed;
+		this.bgOffset = this.$background.offset().top;
+
+		this.bindEvents();
+	}
+
+	get scrollTop() {
+		return window.pageYOffset || document.documentElement.scrollTop;
+	}
+	get scrollFactor() {
+		return (this.scrollTop - this.bgOffset) / this.bgSpeed;
+	}
 	
-	toggleNav() {
-		$('body').toggleClass('nav-toggled');
+	onToggle() {
+		this.$container.toggleClass(this.toggleClass);
+	}
+	onScroll() {
+    const scaleFactor = (this.scrollFactor / 2000) + 1;
+    let transform = `scale(${scaleFactor}) translateY(${this.scrollFactor}px)`,
+    		opacity = 1 - (this.scrollFactor / 100);
+
+		this.$background.css({ transform, opacity });
+	}
+
+	bindEvents() {
+		this.onToggle = this.onToggle.bind(this);
+		this.onScroll = this.onScroll.bind(this);
+
+		this.$toggler.on('click', this.onToggle);
+		this.$window.on('scroll resize', this.onScroll);
+
+		this.$window.trigger('scroll');
 	}
 }
 
-  // document ready
 $(() => {
 	const page = new Page();
-	const navToggle = document.getElementById('navToggle');
-	navToggle.addEventListener('click', page.toggleNav);
+	const nav = new Nav({
+		container: 'body',
+		toggler: '#navToggle',
+		toggleClass: 'nav-toggled',
+		background: '#hero-image',
+		bgSpeed: 8
+	});
 
-	console.log(page);
 });
-
